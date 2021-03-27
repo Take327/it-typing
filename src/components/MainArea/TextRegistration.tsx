df08102abd00a5804dbb1788f82e0831c86fc9f4
Take327 <gatewayseed2@gmail.com>
2021-03-27 23:39:07 +0900

    　

--- src/components/MainArea/TextRegistration.tsx
+++ src/components/MainArea/TextRegistration.tsx
@@ -19,32 +19,6 @@ const TextRegistration = (props: Props) => {
 
     const rows = props.typingText
 
-
-    /*
-    const rows = [
-        {
-            id: 1,
-            kanaText: "いぬもあるけばぼうにあたる",
-            originalText: "犬も歩けば棒に当たる"
-        },
-        {
-            id: 2,
-            kanaText: "test",
-            originalText: "test"
-        },
-        {
-            id: 3,
-            originalText: "豚に真珠",
-            kanaText: "ぶたにしんじゅ"
-        },
-        {
-            id: 4,
-            kanaText: "いしのうえにもさんねん",
-            originalText: "石の上にも三年"
-        }
-    ]
-    */
-
     return (
         <div style={{ height: 400, width: '100%', backgroundColor: '#FFF' }}>
             <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
